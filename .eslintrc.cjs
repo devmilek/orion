2a75cdde780e669a0adf81d5cd75e499ca61f352
Milosz <miloszkrainsky@gmail.com>
2023-02-10 18:49:29 +0100

    added storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,25 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
     root: true,
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-prettier',
+        'plugin:storybook/recommended',
+    ],
     parserOptions: {
         ecmaVersion: 'latest',
     },
     rules: {
         'prettier/prettier': [
             'error',
-            { singleQuote: true, semi: false, printWidth: 100, tabWidth: 4 },
+            {
+                singleQuote: true,
+                semi: false,
+                printWidth: 100,
+                tabWidth: 4,
+            },
         ],
     },
 }
